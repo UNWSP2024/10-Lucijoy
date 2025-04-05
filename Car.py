47c4619d8337f063a57c26b904baee55cd9be858
Lucijoy <ljfloan2@students.unwsp.edu>
2025-04-04 23:09:53 -0500

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,3 +1,7 @@
+# Author: Lucia Floan
+# Date: Apr 4, 2025
+# Title: Car Class
+
 # Program # 2: Car Class
 # Write a class named Car that has the following data attributes:
 
@@ -12,3 +16,29 @@
 # The brake method should subtract 5 from the speed data attribute each time it is called.
 # The get_speed method should return the current speed.
 # Next, design a program that creates a Car object then calls the accelerate method five times.  After each call to the accelerate method, get the current speed of the car and display it.  The call the brake method.  After each call to the brake method, get the current speed of the car and display it.
+
+class Car:
+
+  def __init__(self, year_model, make):
+    self.__year_model = year_model
+    self.__make = make
+    self.__speed = 0
+
+  def accelerate(self):
+    self.__speed += 5
+  def brake(self):
+    self.__speed -= 5
+  def get_speed(self):
+    return self.__speed
+
+my_car = Car(2025, "Porsche")
+print('initial speed:', my_car.get_speed())
+for i in range(5):
+
+  my_car.accelerate()
+  print(f'speed after accelarating {i + 1} times: {my_car.get_speed()}')
+
+for i in range(5):
+  my_car.brake()
+  print(f'speed after breaking {i + 1} times: {my_car.get_speed()}')
+
